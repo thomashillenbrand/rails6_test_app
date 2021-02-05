8629fb95042774c3a87b8714ab1d3e6ac6f2e901
Thomas Hillenbrand <thomashillenbrand16@gmail.com>
2021-02-04 22:41:49 -0500

    Added about page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # define a "root route" controller. "_controller.rb" is implied in the name and not needed to specify controller
   # #<method_name> after controller name will determine what controller method is run.
   root 'pages#home'
+  get 'about', to: 'pages#about' # get request to the <root>/about path --> handled by pages controller and about action.
 end
